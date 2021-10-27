6c9b7e75fde007a2a2c299933ac721c653d0e5e4
Fedon <92860822+Fzenios@users.noreply.github.com>
2021-10-27 16:05:33 +0300

    Testing Branches

---
+++ Taboosti/Assets/Scripts/AllDataHere.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AllData", menuName ="AllData")]
+public class AllDataHere : ScriptableObject
+{
+
+}
